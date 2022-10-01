45b4dc1060068d80486e6727fb9a798e5a8d0533
lexatref <lexatref@gmail.com>
2022-10-01 22:45:33 +0300

    dict

---
+++ python_ds_solution_lesson_2_dict.py
@@ -0,0 +1,47 @@
+
+class CountVectorizerDict:
+    def __init__(self):
+        self.features = {}
+
+    def find_feature_names(self, text: list[str]):
+        self.features.clear()
+        i = 0
+        for text_string in text:
+            words = text_string.lower().split(' ')
+            for word in words:
+                if not (word in self.features):
+                    self.features[word] = i
+                    i += 1
+
+    def fit_transform(self, text: list[str]) -> list[list[int]]:
+        self.find_feature_names(text)
+        result = []
+        for text_string in text:
+            result_string = [0] * len(self.features)
+            words = text_string.lower().split(' ')
+            for word in words:
+                i = self.features[word]
+                result_string[i] += 1
+            result.append(result_string)
+        return result
+
+    def get_feature_names(self) -> list[str]:
+        return list(self.features.keys())
+
+
+def main():
+    corpus = [
+        'Crock Pot Pasta Never boil pasta again',
+        'Pasta Pomodoro Fresh ingredients Parmesan to taste'
+    ]
+
+    vectorizer = CountVectorizerDict()
+    count_matrix = vectorizer.fit_transform(corpus)
+
+    print(vectorizer.get_feature_names())
+
+    print(count_matrix)
+
+
+if __name__ == '__main__':
+    main()
--- tests/test.py
+++ tests/test.py
@@ -1,5 +1,6 @@
 import unittest
 from python_ds_solution_lesson_2 import CountVectorizer
+from python_ds_solution_lesson_2_dict import CountVectorizerDict
 
 
 class MyTestCase(unittest.TestCase):
@@ -21,6 +22,30 @@ class MyTestCase(unittest.TestCase):
         ]
 
         vectorizer = CountVectorizer()
+        vectorizer.fit_transform(corpus)
+        res = vectorizer.get_feature_names()
+        self.assertEqual(res, ['crock', 'pot', 'pasta', 'never', 'boil',
+                               'again', 'pomodoro', 'fresh', 'ingredients',
+                               'parmesan', 'to', 'taste'])
+
+    def test_fit_transform_dict(self):
+        corpus = [
+            'Crock Pot Pasta Never boil pasta again',
+            'Pasta Pomodoro Fresh ingredients Parmesan to taste'
+        ]
+
+        vectorizer = CountVectorizerDict()
+        count_matrix = vectorizer.fit_transform(corpus)
+        self.assertEqual(count_matrix, [[1, 1, 2, 1, 1, 1, 0, 0, 0, 0, 0, 0],
+                                        [0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1]])
+
+    def test_get_feature_names_dict(self):
+        corpus = [
+            'Crock Pot Pasta Never boil pasta again',
+            'Pasta Pomodoro Fresh ingredients Parmesan to taste'
+        ]
+
+        vectorizer = CountVectorizerDict()
         vectorizer.fit_transform(corpus)
         res = vectorizer.get_feature_names()
         self.assertEqual(res, ['crock', 'pot', 'pasta', 'never', 'boil',
